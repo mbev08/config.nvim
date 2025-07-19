06986f5daa28c9c299f55c5d4c60b29135660881
mbev08 <mbeverly2021@gmail.com>
2025-07-19 00:32:03 -0500

    init commit. Added keymaps.

---
+++ lua/config/keymaps.lua
@@ -0,0 +1,71 @@
+-- Keymaps are automatically loaded on the VeryLazy event
+-- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
+--------------------------------------------------------------------
+-- Documentation
+--
+--	-------------------------------
+-- 	Modes
+-- 		(n) Normal
+-- 		(v) Visual
+-- 		(N) Visual Line
+-- 		(x) Visual Block
+-- 		(s) Select - same as visual, but allows you to overwrite selected text.
+-- 		(o) Operator Pending - when vim waits for navigation (e.g. d or y)
+-- 		(i) Insert
+-- 		(c) Command Line - used for entering commands (e.g. :, /, ?)
+-- 		(t) Terminal - used for interacting with the terminal inside vim
+--
+
+--------------------------------------------------------------------
+-- Leader
+vim.g.mapleader = " "
+
+--------------------------------------------------------------------
+-- Shortcuts - Vim Motions
+-------------------------------
+---- Default Mode
+
+-- copy to clipboard
+vim.keymap.set({ "n", "v" }, "<leader>y", [["+y]])
+
+-- copy to clipboard (entire line)
+vim.keymap.set("n", "<leader>Y", [["+Y]])
+
+-- paste over text but don't record in registry
+vim.keymap.set("x", "<leader>p", [["_dP]])
+
+-- delete but don't record in registry.
+vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
+
+-- overwrite CTRL+Z to Redo.
+vim.keymap.set("n", "<C-z>", "<C-r>")
+
+-- join line below current line, then return to original cursor position.
+vim.keymap.set("n", "J", "mzJ`z")
+
+-- cancel highlighted search matches
+vim.keymap.set("n", "<leader>/", "<cmd>:nohls<CR>")
+
+-- scroll up and center screen
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+
+-- scroll down and center screen
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- format syntax using LSP
+vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
+
+-------------------------------
+---- Visual Mode
+
+-- shift selected text down
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- shift selected text up
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+--------------------------------------------------------------------
+-- Shortcuts - Call Other ...
+vim.keymap.set("n", "<leader>pv", vim.cmd.Ex)
+
+-- test tt fs
---
+++ lua/config/options.lua
@@ -0,0 +1,37 @@
+-- Options are automatically loaded before lazy.nvim startup
+-- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
+-- Add any additional options here
+
+vim.opt.nu = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+
+vim.opt.swapfile = false
+vim.opt.backup = false
+vim.opt.undodir = os.getenv("HOME") .. "/.vim/undodir"
+
+vim.opt.hlsearch = true
+vim.opt.incsearch = true
+
+vim.opt.scrolloff = 8
+vim.opt.isfname:append("@-@")
+
+vim.opt.updatetime = 50
+
+vim.opt.colorcolumn = "80"
+
+vim.wo.foldmethod = "expr"
+--vim.wo.foldexpr = "v:lua.vim.treesitter.foldexpr()"
+--vim.opt.foldlevelstart = 99
+
+--function _G.CustomFoldText()
+--  local start_line = vim.fn.getline(vim.v.foldstart)
+--  local end_line = vim.fn.getline(vim.v.foldend):gsub("^%s*", "")
+--  return start_line .. " ... " .. end_line
+--end
+--vim.opt.foldtext = "v:lua.CustomFoldText()"
+
+--gold
+--vim.api.nvim_set_hl(0, "Folded", { fg = "gold", bg = "black" })
